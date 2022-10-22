f78605ef5dd24493cf463008171748bea4ca21a2
ishah148 <70168146+ishah148@users.noreply.github.com>
2022-10-22 12:59:22 +0300

    add SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <input type="search" v-model="searchQuery" />
+  <button class="button-primary" @click="updateText">123</button>
+  <p>{{ text }}</p>
+  <p>{{ searchQuery }}</p>
+  <p>{{ test2.fname }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+      text: '',
+      searchQuery: '',
+      test2: {
+        name: '2',
+        fname: 2
+      }
+    }
+  },
+  watch: {},
+  methods: {
+    updateText () {
+      this.test2.fname = 3
+    }
+  }
+})
+</script>
+
+<style scoped>
+.button-primary {
+  height: 30px;
+  width: 50px;
+}
+</style>
