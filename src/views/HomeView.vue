f626de699c1947e097ee08c4ce583a5dca01ebbf
ishah148 <70168146+ishah148@users.noreply.github.com>
2022-10-22 13:03:42 +0300

    create main page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <HeaderMain>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </HeaderMain>
+    <ResourcesItemList />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HeaderMain from '@/components/HeaderMain.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import ResourcesItemList from '@/components/ResourcesItemList.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HeaderMain,
+    ResourcesItemList
+  },
+  mounted () {
+    console.log('Main is mounted')
+  }
+})
+</script>
