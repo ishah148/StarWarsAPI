597e37d0bac556a5694fb4dc828ae5148a9249e3
ishah148 <70168146+ishah148@users.noreply.github.com>
2022-10-23 19:54:37 +0300

    MyImage: Fix img patch creator

--- src/components/MyImage.vue
+++ src/components/MyImage.vue
@@ -1,7 +1,10 @@
 <template>
-  <img :src="src" alt="" />
+  <div>
+    <img v-if="src" :src="src" alt="" />
+  </div>
 </template>
 <script lang="ts">
+import { resources } from '@/models/SwapApi/resources'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -16,14 +19,17 @@ export default defineComponent({
   },
   methods: {
     createSrc () {
-      console.log(
-        'fff',
-        `img/people/${this.url?.split('/').slice(-2, -1)[0]}.jpg`
-      )
-      return `img/people/${this.url?.split('/').slice(-2, -1)[0]}.jpg`
+      const group = this.defineGroup()
+      console.log('group', group)
+      if (!group) return ''
+      return `img/${group}/${this.url?.split('/').slice(-2, -1)[0]}.jpg`
+    },
+    defineGroup () {
+      return this.url?.split('/').filter((i) => resources.includes(i))[0]
     }
   },
   mounted () {
+    console.log('url', this.url)
     this.src = this.createSrc()
   },
   updated () {
