027a505abd89b9bac8a403566ed6da9d779ae161
Igor Shah <zombee1992@gmail.com>
2022-10-26 13:58:51 +0300

    add style, change router handler

--- vue-project/src/components/DescriptionItems/FullDescriptionItem.vue
+++ vue-project/src/components/DescriptionItems/FullDescriptionItem.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="description__container" v-if="Object.keys(obj || {}).length">
     <div class="description__info-container">
-      <MyImage class="description__info__image" :url="obj?.url" />
+      <MyImage class="description__info-image" :url="obj?.url" />
       <div class="description__text-container">
         <p class="description__text" v-for="(item, index) in strings" :key="item">{{ index }} : {{ item }}</p>
       </div>
     </div>
-    <p>--------------</p>
-    <!-- <div v-for="(item, index) in urls" :key="item">
-      <p :key="item">{{ index }} : {{ item }}</p>
-    </div> -->
-    <p>--------------</p>
-    <div v-for="(item, index) in arrays" :key="item">
-      <p :key="item">{{ index }} : {{ item }}</p>
-      <p v-for="(url, index) in item" :key="index">url{{ url }}
-        <MyImage class="test" :url="url" @click="redirect(url)" />
-         <!-- <router-link class="link" to="/people/details/1">Vehicles</router-link> -->
-      </p>
+
+    <div class="description-links__container" v-for="(item, index) in arrays" :key="index">
+      <p class="description-links__group" :key="item">{{ index }}: <br></p>
+      <div class="description-links__image-container" v-for="(url, index) in item" :key="index">
+        <router-link class="link" :to="{
+          name: 'details', params: { group: getGroupUrl(url), id: getIdUrl(url) }
+        }">
+          <MyImage class="description-links__image" :url="url" />
+        </router-link>
+      </div>
     </div>
   </div>
 
 </template>
 <script lang="ts">
 import { SwapApiData } from '@/models/SwapApi/resources'
-import { getValidPath } from '@/utils/url_helper'
+import { defineGroup, defineId, getValidPath } from '@/utils/url_helper'
 import { defineComponent, PropType } from 'vue'
 import MyImage from '../MyImage.vue'
 export default defineComponent({
@@ -44,10 +43,6 @@ export default defineComponent({
       const filteredArr = Object.entries(this.obj || {}).filter(
         (i) => Array.isArray(i[1]) && i[1].length > 1
       )
-      filteredArr.forEach(item => {
-        // (item[1] as string[]).
-      })
-
       this.arrays = Object.fromEntries(filteredArr)
 
       const filteredUrls = Object.entries(this.obj || {}).filter((i) => {
@@ -72,16 +67,21 @@ export default defineComponent({
     },
     redirect(url: string) {
       const newUrl = getValidPath(url)
-      // TODO to correct !!!
-      this.$router.push({ path: '/' + newUrl })
-      this.$router.go(1)
-      setTimeout(() => {
-        window.history.go()
-      }, 10);
+      this.$router.push({
+        name: 'details',
+        params: {
+          group: defineGroup(newUrl),
+          id: defineId(newUrl)
+        },
+      })
     },
-    getPathUrl(url: string) {
-      return getValidPath(url)
-    }
+    getGroupUrl(url: string) {
+      return defineGroup(url)
+    },
+    getIdUrl(url: string) {
+      return defineId(url)
+    },
+
   },
   mounted() {
     this.filterData()
@@ -97,35 +97,72 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-  .description__container{
-    max-width: 1200px;
-    width: 100%;
-    margin: auto;
-    margin-top: 35px;
-    font-weight: normal;
+.description__container {
+  max-width: 1200px;
+  width: 100%;
+  margin: auto;
+  margin-top: 35px;
+  font-weight: normal;
+  padding: 25px;
+
+  p {
+    margin: 0;
   }
-  .description__info-container {
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    .description__info__image {
-      // flex: 1;
-    }
-    .description__text-container {
-      // flex: 1;
-      .description__text {
-        text-align: start;
-        font-weight: normal;
-      }
+}
+
+.description__info-container {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  gap: 30px;
+
+  .description__info-image {
+    // flex: 1;
+    width: 300px;
+
+    img {
+      width: 300px;
     }
   }
 
-  .test{
-    width: 50px;
-    height: 50px;
-    img{
-      border-radius: 50%;
+  .description__text-container {
+
+    // flex: 1;
+    .description__text {
+      text-align: start;
+      font-weight: normal;
+      margin-top: 15px;
     }
   }
 
+  .description-links__container {}
+}
+
+.description-links__container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  row-gap: 3px;
+  column-gap: 15px;
+  margin-top: 25px;
+  justify-content: center;
+  .description-links__group {
+    width: 100%;
+    margin: 5px;
+  }
+}
+
+.description-links__image {
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  img {
+    border-radius: 50%;
+  }
+}
 </style>
